4e0202605b607a9f1b377a12aa58548d5ae9264c
Tomas Zubik <zubik.tomas@gmail.com>
2022-02-23 20:57:58 -0300

    initial commit

---
+++ main_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def search(request):
+    return render(request, 'search.html')
+
+
+def product(request):
+    return render(request, 'product.html')
